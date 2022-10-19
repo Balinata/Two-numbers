669decaab68708ce69bf38d72fd5cfacd734a513
natalia <devidvarl@gmail.com>
2022-10-19 16:22:30 +0200

    format selection

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let a = Number(prompt('Введіть перше число'));
-let b = Number(prompt('Введіть друге число')); 
+let b = Number(prompt('Введіть друге число'));
 let c;
 c = a + b;
 alert((c));
-alert(typeof c );
+alert(typeof c);
